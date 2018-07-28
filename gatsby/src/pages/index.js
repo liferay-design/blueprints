1461651db6040bfe8baaea578501a88be3302ad8
Ryan Garant <ryantgarant@gmail.com>
2018-07-27 17:40:18 -0700

    Add markdown files to index page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,14 +1,40 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <h1>Amazing Pandas Eating Things</h1>
     <div>
-      <img
-        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-        alt="Group of pandas eating bamboo"
-      />
+      <h1>Amazing Pandas Eating Things</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title}{' '}
+            <span>— Time to read: {node.timeToRead}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+          }
+          excerpt
+          timeToRead
+        }
+      }
+    }
+  }
+`
